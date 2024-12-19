7a7e9e9d8ac1a1e6b2895a55d1085f6b4392a57d
sidhantrajput1 <sidhant808310@gmail.com>
2024-12-19 23:49:41 +0530

    print and reverse

---
+++ Queue/print.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+
+using namespace std;
+
+void print(queue<int>& a) {
+    int n = a.size();
+    for(int i = 1; i <= n ; i++) {
+        int x = a.front();
+        cout<<x<<" ";
+        a.pop();
+        a.push(x);
+    }
+    cout<<endl;
+}
+
+void reverse(queue<int>& a) {
+    stack<int> st;
+    while(a.size() > 0) {
+        int x = a.front();
+        a.pop();
+        st.push(x);
+    }
+
+    while(st.size() > 0) {
+        int x = st.top();
+        st.pop();
+        a.push(x);
+    }
+}
+
+int main() {
+    queue<int> a;
+    a.push(1);
+    a.push(2);
+    a.push(3);
+    a.push(4);
+    a.push(5);
+    print(a);
+    reverse(a);
+    print(a);
+}
