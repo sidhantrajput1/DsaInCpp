12486b8c42500fae4804ae3f246898616d80286c
sidhantrajput1 <sidhant808310@gmail.com>
2025-01-14 17:49:55 +0530

    bfs traversal left to right and right to left

--- BinaryTree 2/ProOrder.cpp
+++ BinaryTree 2/ProOrder.cpp
@@ -23,9 +23,46 @@ void preorder(TreeNode* root) {
 // print element of nth level
 void printNthLevelOfElement(TreeNode* root, int curr, int level) {
     if(root==NULL) return;
-    if(curr == level) cout<<root->val<<" ";
+    if(curr == level) {
+        cout<<root->val<<" ";
+        return;
+    }
     printNthLevelOfElement(root->left, curr+1, level);
     printNthLevelOfElement(root->right, curr+1, level);
+}
+
+void printNthLevelOfElementInReverse(TreeNode* root, int curr, int level) {
+    if(root==NULL) return;
+    if(curr == level) {
+        cout<<root->val<<" "; // root
+        return;
+    }
+    printNthLevelOfElementInReverse(root->right, curr+1, level); // right
+    printNthLevelOfElementInReverse(root->left, curr+1, level); // left
+}
+
+
+// find the level fo tree
+int levels(TreeNode* root) {
+    if(root==NULL) return 0;
+    return 1 + max(levels(root->left) , levels(root->right));
+}
+
+// bfs traversal
+void leveltraversal(TreeNode* root) {
+    int n = levels(root);
+    for(int i = 1; i <= n; i++) {
+        printNthLevelOfElement(root, 1, i);
+        cout<<endl;
+    }
+}
+
+void leveltraversalReverse(TreeNode* root) {
+    int n = levels(root);
+    for(int i = 1; i <= n; i++) {
+        printNthLevelOfElementInReverse(root, 1, i);
+        cout<<endl;
+    }
 }
 
 int main() {
@@ -47,4 +84,10 @@ int main() {
     preorder(a);
     cout<<endl;
     printNthLevelOfElement(a, 1, 3);
+    cout<<endl;
+
+    // level traversal
+    leveltraversal(a);
+    cout<<endl;
+    leveltraversalReverse(a); 
 }
