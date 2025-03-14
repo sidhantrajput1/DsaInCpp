a6d255fba49f8f4b5b3090718f50f2b9c3af78b4
sidhantrajput1 <sidhant808310@gmail.com>
2025-03-14 09:36:22 +0530

    string

---
+++ String2/stoi_stol.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main () {
+    // change string to int
+    string str = "1234567";
+    int x = stoi(str);
+    cout<<x;
+
+    // change int to string
+    // int a = 123456;
+    // string s = to_string(a);
+
+} 
